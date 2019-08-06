9b4aafd0b92c622cebe09cd91355ac097221ae50
PaengE <ckddn2820@gmail.com>
2019-08-06 13:20:30 +0900

    02

---
+++ 02/2884.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+	int h, m;
+	scanf("%d %d", &h, &m);
+	
+	if(h >= 0 && h <= 23 && m >= 0 && m <= 59){
+		if(m - 45 < 0){
+			if(h == 0){
+				h = 23;
+				m = 60 + (m - 45);
+			} else {
+				h--;
+				m = 60 + (m - 45);
+			}
+		} else {
+			m = m - 45;
+		}
+		printf("%d %d", h, m);
+	}
+}
