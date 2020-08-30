a74f8e7074052e4964a1d0a38c442612738cb499
cw <ckddn2820@gmail.com>
2020-08-30 23:46:38 +0900

    실습1

---
+++ coding_python/10039.py
@@ -0,0 +1,4 @@
+sum = 0
+for i in range(5):
+    sum += max(40, int(input()))
+print(sum//5)
---
+++ coding_python/5543.py
@@ -0,0 +1,7 @@
+a = 2001
+b = 2001
+for i in range(3):
+    a = min(a, int(input()))
+for j in range(2):
+    b = min(b, int(input()))
+print(a+b-50)
