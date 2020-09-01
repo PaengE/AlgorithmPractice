ed1ac434ebd67f0334688a5995656dfb7322592c
cw <ckddn2820@gmail.com>
2020-09-02 00:26:11 +0900

    java setting

---
+++ coding_java/src/BOJ15649.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BOJ15649 {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+
+        int arr[] = new int[M];
+
+
+    }
+}
