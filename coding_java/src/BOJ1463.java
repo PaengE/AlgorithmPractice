542ad1a586c3bd679b0e5834df44de54483c8774
cw <ckddn2820@gmail.com>
2020-09-08 20:35:40 +0900

    DP

---
+++ coding_java/src/BOJ1463.java
@@ -0,0 +1,47 @@
+import java.io.*;
+
+/*
+    no.1463 : 1로 만들기
+    hint : -1, /2, /3 중 작은걸 선택
+ */
+
+public class BOJ1463 {
+    static int n;
+    static int[] arrDP;
+
+    public static void dp() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        if (n == 1){
+            bw.write("0");
+        } else if(n == 2 || n == 3){
+            bw.write("1");
+        } else{
+            arrDP[1] = 0;
+            arrDP[2] = 1;
+            arrDP[3] = 1;
+            for(int i = 4; i <= n; i++){
+                arrDP[i] = arrDP[i-1] + 1;
+                if(i % 2 == 0){
+                    arrDP[i] = Math.min(arrDP[i], arrDP[i / 2] + 1);
+                }
+                if(i % 3 == 0){
+                    arrDP[i] = Math.min(arrDP[i], arrDP[i / 3] + 1);
+                }
+            }
+            bw.write(arrDP[n] + "");
+        }
+        bw.flush();
+        bw.close();
+
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        arrDP = new int[n+1];
+
+        dp();
+
+        br.close();
+    }
+}
