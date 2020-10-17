a88d511e0b183dfa5f1a30c3bf1935f37685fa79
cw <ckddn2820@gmail.com>
2020-10-17 23:44:25 +0900

    DP 2: 11066

--- coding_java/src/BOJ11066.java
+++ coding_java/src/BOJ11066.java
@@ -1,6 +1,16 @@
 import java.io.*;
-import java.util.Arrays;
 import java.util.StringTokenizer;
+
+/**
+ * no.11066: 파일 합치기
+ * description: 파일을 합쳐 하나로 모으는 최소 비용을 구하는 문제
+ * hint: Knuth Optimization ?
+ *      1. 무조건 인접한 것끼리 합쳐야 함
+ *      2. 규칙상 마지막 연산은 전체 합을 더해야 함
+ * 점화식: n == 1일 때, DP[i][i] = 0
+ *         n == 2일 때, DP[i][i + 1] = V[i] + V[i + 1]
+ *         n >= 3일 때ㅡ DP[i][j] = MIN(DP[i][j], DP[i][k] + DP[k + 1][j] + SUM(V[i ~ j]) (단, i < k < j)
+ */
 
 public class BOJ11066 {
     public static void main(String[] args) throws IOException {
@@ -11,52 +21,59 @@ public class BOJ11066 {
         StringTokenizer st = null;
         for (int i = 0; i < t; i++) {
             int k = Integer.parseInt(br.readLine());
+            int[] file = new int[k];
             st = new StringTokenizer(br.readLine());
-            int[] arr = new int[k];
+
             for (int j = 0; j < k; j++) {
-                arr[j] = Integer.parseInt(st.nextToken());
-            }
-            Arrays.sort(arr);
-
-            int[][] dp = new int[k][k];
-            int idx = 0;
-            long sum = 0;
-            for (int j = 1; j < k; j += 2) {
-                dp[0][idx++] = arr[j - 1] + arr[j];
-                System.out.print(" " + dp[0][idx-1]);
-                sum += dp[0][idx - 1];
-            }
-            if (k % 2 == 1) {
-                dp[0][idx] = arr[k - 1];
-                System.out.print(" " + dp[0][idx]);
-                sum += dp[0][idx];
-            }
-            System.out.println();
-
-
-            // 홀수면 안더하고 나중에 더하면 됨
-            int tmpIdx = 0;
-            var ceil = Math.ceil(Math.log(k) / Math.log(2));
-            for (int j = 1; j < (int) ceil; j++) {
-                int tmp = 1;
-                while (dp[j - 1][tmp] != 0) {
-                    dp[j][tmpIdx++] = dp[j - 1][tmp - 1] + dp[j - 1][tmp];
-                    System.out.print(" " + dp[j][tmpIdx-1]);
-                    sum += dp[j][tmpIdx - 1];
-                    tmp += 2;
-                }
-                if (dp[j - 1][tmp - 1] != 0) {
-                    dp[j][tmpIdx] = dp[j - 1][tmp - 1];
-                    System.out.print(" " + dp[j][tmpIdx]);
-                    sum += dp[j][tmpIdx];
-                }
-                System.out.println();
+                file[j] = Integer.parseInt(st.nextToken());
             }
 
-            bw.write(sum + "\n");
+            bw.write(String.valueOf(solution(file)));
+            bw.newLine();
         }
         bw.flush();
         bw.close();
         br.close();
+    }
+
+    static int solution(int[] file) {
+        int size = file.length;
+        int[][] dp = new int[size][size];
+        int[] sum = new int[size];
+
+        sum[0] = file[0];
+
+        // 앞에서부터 순서대로 파일을 합친 값
+        for (int i = 1; i < sum.length; i++) {
+            sum[i] = sum[i - 1] + file[i];
+        }
+
+        // 앞에서부터 인접한 두 파일을 합친 값
+        for (int i = 0; i < file.length - 1; i++) {
+            dp[i][i + 1] = file[i] + file[i + 1];
+        }
+
+        // 파일의 개수가 >= 3일 때
+        for (int gap = 2; gap < size; gap++) {
+            for (int i = 0; i + gap < size; i++) {  // i index
+                int j = i + gap;                    // j index
+                dp[i][j] = Integer.MAX_VALUE;
+
+                // i ~ j 사이의 k 값
+                for (int k = i; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j], dp[i][k] + dp[k + 1][j] + sumDist(sum, i, j));
+                }
+            }
+        }
+        return dp[0][size - 1];
+    }
+
+    // start 부터 end 까지의 파일의 합을 구하는 메소드
+    static int sumDist(int[] sum, int start, int end) {
+        if (start == 0) {
+            return sum[end];
+        } else {
+            return sum[end] - sum[start - 1];
+        }
     }
 }
