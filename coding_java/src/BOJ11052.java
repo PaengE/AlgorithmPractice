b47ebe34415f9da4bc5abc664ceee7eafe172711
PaengE <https://github.com/PaengE/AlgorithmPractice>
2020-11-18 18:43:45 +0900

    push test

--- coding_java/src/BOJ11052.java
+++ coding_java/src/BOJ11052.java
@@ -1,5 +1,11 @@
-public class BOJ11052 {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class BOJ11052 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        br.close();
     }
 }
