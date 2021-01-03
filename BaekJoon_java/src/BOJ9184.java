83807447513fbf8964319114891570aa25603a52
PaengE <ckddn2820@gmail.com>
2021-01-04 00:11:00 +0900

    DP: 9184

---
+++ BaekJoon_java/src/BOJ9184.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+/**
+ *  no.9184: 신나는 함수 실행
+ *  url: https://www.acmicpc.net/problem/9184
+ *  Hint: DP
+ */
+
+public class BOJ9184 {
+    static int[][][] dp;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        dp = new int[51][51][51];
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+
+            if (a == -1 && b == -1 && c == -1) {
+                break;
+            }
+
+            sb.append("w(" + a + ", " + b + ", " + c + ") = " + w(a, b, c) + "\n");
+        }
+
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
+    }
+
+    static int w(int a, int b, int c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            return 1;
+        }
+
+        if (a > 20 || b > 20 || c > 20) {
+            return w(20, 20, 20);
+        }
+
+        if (dp[a][b][c] != 0) {
+            return dp[a][b][c];
+        }
+
+        if (a < b && b < c) {
+            dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+        } else {
+            dp[a][b][c] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+        }
+        return dp[a][b][c];
+    }
+}
