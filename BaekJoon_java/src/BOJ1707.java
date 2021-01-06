e2f3580bd111714f8b5b11fb09049c91ab9c3e75
PaengE <ckddn2820@gmail.com>
2021-01-07 00:29:17 +0900

    update comments

--- BaekJoon_java/src/BOJ1707.java
+++ BaekJoon_java/src/BOJ1707.java
@@ -33,8 +33,11 @@ public class BOJ1707 {
             }
 
             boolean flag = true;
+            // 연결그래프가 아닐 수도 있으므로 탐색하지 않은 모든 노드 탐색
             for (int i = 1; i <= v; i++) {
                 if (numbering[i] == 0) {
+                    // 인접한 한 덩어리의 노드들로 이분그래프를 만들지 못한다면
+                    // 다른 노드들을 탐색할 필요가 없다.(어차피 만들지 못하므로)
                     if (!bfs(i, e)) {
                         flag = false;
                         break;
@@ -61,11 +64,13 @@ public class BOJ1707 {
             int cur = q.poll();
 
             for (int next : graph.get(cur)) {
+                // 방문하지 않았던 노드라면 현재노드의 번호와 비교하여 다음 노드의 번호를 매김
                 if (numbering[next] == 0) {
                     q.offer(next);
                     numbering[next] = (numbering[cur] == 1) ? 2 : 1;
                 }
 
+                // 다음노드와 현재노드의 번호가 같다면 이분그래프를 만들 수 없다
                 if (numbering[cur] == numbering[next]) {
                     return false;
                 }
