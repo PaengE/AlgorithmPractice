b0621accd27205ee977f7f9c75eb8b4d2a59e021
PaengE <ckddn2820@gmail.com>
2021-01-17 22:53:55 +0900

    add comment

--- programmers/src/Compression.java
+++ programmers/src/Compression.java
@@ -23,18 +23,25 @@ public class Compression {
         for (int i = 0; i < msg.length(); i++) {
             String s = String.valueOf(msg.charAt(i));
             int j = i;
+            // 현재 s가 hashmap에 존재한다면 계속하여 문자열을 붙여서 구성해나감
             while (map.containsKey(s)) {
                 if (++j >= msg.length()) {
                     break;
                 }
                 s += String.valueOf(msg.charAt(j));
             }
+            // 다음 시작은 hashmap에 없는 문자부터 시작해야하므로
             i = j - 1;
 
+            // 마지막 문자까지 않았을 경우
             if (j != msg.length()) {
+                // hashmap에 put한 후, 출력 리스트에 저장함
                 map.put(s, index++);
                 list.add(map.get(s.substring(0, s.length() - 1)));
-            } else {
+            }
+            // 마지막 문자까지 갔을 경우
+            else {
+                // 출력리스트에 저장함
                 list.add(map.get(s));
             }
         }
