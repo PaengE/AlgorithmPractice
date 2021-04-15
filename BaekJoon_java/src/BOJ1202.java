adfda622b5d73e7e2b780297a90f334a05c57090
PaengE <ckddn2820@gmail.com>
2021-04-16 01:41:47 +0900

    1202

--- BaekJoon_java/src/BOJ1202.java
+++ BaekJoon_java/src/BOJ1202.java
@@ -11,7 +11,6 @@ public class BOJ1202 {
     static int n, m;
     static int[] bags;
     static Jewelry[] jewelries;
-    static PriorityQueue<Jewelry> pq;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
